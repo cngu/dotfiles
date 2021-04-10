fe1d473106d82287d2e6b50d0fd30c289e6a59ca
cngu <cngu@users.noreply.github.com>
2021-04-10 13:27:54 -0600

    zsh: Configure menu selection color

--- .shared_shell_scripts/zsh/autocompletion.zsh
+++ .shared_shell_scripts/zsh/autocompletion.zsh
@@ -6,5 +6,8 @@ source $HOME/.shared_shell_scripts/zsh/completion/npm
 # Highlight selection background as you tab through autocomplete options
 zstyle ':completion:*' menu select
 
-# Highlight prefix/text of autocomplete options that are already typed
-zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)*==34=34}:${(s.:.)LS_COLORS}")';
+# Use LS_COLORS (defined in .zshrc) to match ls colors.
+# Highlight prefix/text of autocomplete options that are already typed.
+# ma is a special attribute for zsh's menu selection color
+# https://gist.github.com/thomd/7667642
+zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)(?)*==34=34}:${(s.:.)LS_COLORS}:ma=100")';
